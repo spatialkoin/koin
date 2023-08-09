83ec3807626371136d89b3d2a6c67412ffaa10c6
TFNLab <tfnlab@gmail.com>
2023-08-09 00:22:09 -0700

    update

--- koin.py
+++ koin.py
@@ -1,6 +1,14 @@
 import socket
 import threading
 import miniupnpc
+import os
+import hashlib
+import signal
+
+# Global variable to track the server socket
+server_socket = None
+upnp = None
+files = "../files/"
 
 def handle_client(client_socket):
     while True:
@@ -11,20 +19,58 @@ def handle_client(client_socket):
             decoded_data = data.decode('utf-8')
             print("Received:", decoded_data)
 
-            response = "Message received: " + decoded_data
+            command, *rest = decoded_data.split(maxsplit=1)
+            if command == "GET":
+                file_name = files + rest[0]
+                if os.path.exists( file_name):
+                    with open( file_name, 'r') as file:
+                        file_content = file.read()
+                        response = "File content:\n" + file_content
+                else:
+                    response = "File not found"
+            else:
+                file_hash = hashlib.sha256(decoded_data.encode('utf-8')).hexdigest()
+                file_name = files + file_hash + ".txt"
+                with open(file_name, 'w') as file:
+                    file.write(decoded_data)
+                response = "Data saved to file with hash as name: " + file_name
+
             client_socket.send(response.encode('utf-8'))
         except Exception as e:
             print("Error:", e)
             break
     client_socket.close()
 
+def cleanup_and_exit(signum, frame):
+    global server_socket, upnp
+
+    print("Received termination signal. Cleaning up and exiting...")
+
+    if upnp:
+        upnp.deleteportmapping(port, 'TCP')
+        upnp.mappedaddress = ''  # Release the mapped IP address
+
+    if server_socket:
+        server_socket.close()
+
+    # You can add additional cleanup code here if needed
+
+    print("Cleanup complete. Exiting.")
+    exit(0)
+
 def main():
-    host = ''       # Listen on all available network interfaces
-    port = 12345    # Choose a port number
+    global server_socket
+
+    host = ''
+    port = 12345
+
+    # Set up signal handling for termination
+    signal.signal(signal.SIGINT, cleanup_and_exit)
+    signal.signal(signal.SIGTERM, cleanup_and_exit)
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(5)  # Allow up to 5 client connections in the queue
+    server_socket.listen(5)
 
     print(f"Server listening on {host}:{port}")
 
