9f888127b1cae63f7bdcd0f3b3ef0cd26f515884
TFNLab <tfnlab@gmail.com>
2023-08-17 23:05:05 -0700

    update

--- koin.py
+++ koin.py
@@ -17,7 +17,7 @@ register = "../register/"
 files = "../files/"
 document_index = '../document_index.pkl'
 
-end_of_message_indicator = b'\r STOP \r'
+end_of_message_indicator = b'\r KOINSTOP \r'
 
 class DocumentIndex:
     def __init__(self, index_file_path, model_directory):
